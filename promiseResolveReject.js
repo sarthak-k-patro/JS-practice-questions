1c9c63086ce2b9f55019b251f365a517fb84429b
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-02-25 11:49:18 +0530

    corresponding async await

--- promiseResolveReject.js
+++ promiseResolveReject.js
@@ -1,7 +1,7 @@
 let arr = [1, 2, 3, 564546];
-let sum = 0;
 
 let promise = new Promise((resolve, reject) => {
+  let sum = 0;
   if (arr.length < 3) reject("Array length is less than 3");
 
   for (let val = 0; val < arr.length; val++) {
@@ -11,3 +11,24 @@ let promise = new Promise((resolve, reject) => {
 });
 
 promise.then((result) => console.log(result)).catch((err) => console.log(err));
+
+// corresponding async await code
+
+const asyncAwait = async () => {
+  let sum = 0;
+  try {
+    let promise = new Promise((resolve, reject) => {
+      if (arr.length < 3) reject("Array length is less than 3");
+
+      for (let val = 0; val < arr.length; val++) {
+        sum = sum + arr[val];
+      }
+      resolve(`sum is: ${sum}`);
+    });
+    const output = await promise;
+    console.log(output);
+  } catch (err) {
+    console.log(err);
+  }
+};
+asyncAwait();
